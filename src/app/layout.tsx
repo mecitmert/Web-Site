d147512e8183db8e431d2ff1188f7b634f699964
İrem Demir <95531247+IRMEDEMIR@users.noreply.github.com>
2025-09-05 01:25:57 +0300

    Feature: Background eklendi.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Footer from "@/components/Footer";
 import PillNav from "@/components/navbar";
+import DarkVeil from "@/components/Background";
 
 export const metadata: Metadata = {
   title: "MMB Yazılım | Web, SEO, Performans",
@@ -21,38 +22,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     { label: "Blog", href: "/blog" },
     { label: "İletişim", href: "/iletisim" },
   ];
+
   return (
     <html lang="tr">
-      <body className="min-h-dvh bg-white text-slate-900">
-        {/*
-         Header 
-        <header className="sticky top-0 z-10 border-b bg-white/80 backdrop-blur">
-          <nav className="mx-auto flex max-w-6xl items-center justify-between p-4">
-            <a className="font-bold">MMB Yazılım</a>
-            <div className="flex gap-4 text-sm">
-              <a href="/">Ana Sayfa</a>
-              <a href="/hizmetler">Hizmetler</a>
-              <a href="/blog">Blog</a>
-              <a
-                href="/iletisim"
-                className="rounded-xl border px-3 py-1"
-              >
-                İletişim
-              </a>
-            </div>
-          </nav>
-        </header>
-        */}
+      <body className="relative min-h-screen overflow-x-hidden text-white">
+        {/* Arka plan */}
+        <div className="fixed inset-0 z-0">
+          <DarkVeil />
+        </div>
 
-        {/* Navbar */}
-        <PillNav logo="/logo.svg" items={navItems} />
-
-
-        {/* Page content */}
-        <main className="mx-auto max-w-6xl p-4">{children}</main>
-
-        {/* Footer component */}
-        <Footer />
+        {/* İçerik */}
+        <div className="relative z-10">
+          <PillNav logo="/logo.svg" items={navItems} />
+          <main className="mx-auto max-w-6xl p-4">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
